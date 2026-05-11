152b7d31220070b42f4a28a5e74c54e8b9aa95c8
CY.LEE <u8901006@gmail.com>
2026-05-11 10:09:37 +0800

    🐛 Fix XML parser + add .gitignore, remove node_modules from tracking

--- test_parse.cjs
+++
@@ -1,8 +0,0 @@
-const { XMLParser } = require("fast-xml-parser");
-const parser = new XMLParser({
-  ignoreAttributes: false,
-  isArray: (name) => name === "PubmedArticle" || name === "AbstractText" || name === "Keyword",
-});
-const xml = '<PubmedArticleSet><PubmedArticle><MedlineCitation><PMID Version="1">12345</PMID><Article><Journal><Title>Test Journal</Title><JournalIssue><PubDate><Year>2026</Year><Month>May</Month></PubDate></JournalIssue></Journal><ArticleTitle>Test Title</ArticleTitle><Abstract><AbstractText Label="BG">Some text</AbstractText></Abstract></Article></MedlineCitation></PubmedArticle></PubmedArticleSet>';
-const result = parser.parse(xml);
-console.log(JSON.stringify(result.PubmedArticleSet.PubmedArticle[0], null, 2));
